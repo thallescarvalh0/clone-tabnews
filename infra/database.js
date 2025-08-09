09eb32dda3bc495be5b3021294bb1e6d1d2ec446
Thalles Carvalho <thallescarvalhoferreira@gmail.com>
2025-08-09 13:49:26 +0000

    add database.js and .env files

---
+++ infra/database.js
@@ -0,0 +1,21 @@
+import { Client } from "pg";
+
+async function query(queryObjects){
+  
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
+  await client.connect();
+  const result = await client.query(queryObjects);
+  await client.end();
+  return result;}
+
+
+export default{
+  query: query,
+};
