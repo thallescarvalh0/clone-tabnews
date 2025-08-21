525f07536d9a2353529ea89e542ba180ca513f1d
Thalles Carvalho <thallescarvalhoferreira@gmail.com>
2025-08-21 00:17:54 +0000

    make database robusst to error handling and finish endpoint status

--- infra/database.js
+++ infra/database.js
@@ -6,14 +6,21 @@ async function query(queryObjects){
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
 
   await client.connect();
-  const result = await client.query(queryObjects);
-  await client.end();
-  return result;}
+
+  try {
+    const result = await client.query(queryObjects);
+    return result;
+  } catch (error) {
+    console.error("Database query error:", error);
+  } finally {
+    await client.end();
+  }
+}
 
 
 export default{
